3ae84f71526faee6ac0c450b1abf4c32c4ed4a1a
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-12-24 15:49:50 +0100

    responsive main

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menuIcon = document.querySelector(".menu-icon");
 const sidebar = document.querySelector(".sidebar");
+const container = document.querySelector(".container-main");
 
 menuIcon.onclick = function(){
     sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
 }
